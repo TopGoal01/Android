f2452c96f19071513c63f50d371fdec9281ce9dc
sea1hee <sea11hee@gmail.com>
2021-07-28 14:32:48 +0900

    [update] playlist의 videoList null 가지지 않게 변경

--- app/src/main/java/com/example/topgoal/model/Playlist.kt
+++ app/src/main/java/com/example/topgoal/model/Playlist.kt
@@ -4,5 +4,5 @@ data class Playlist(
         var id: String,
         var name: String? = null,
         var count: Int = 0,
-        var videoList: MutableList<Video>? = null
+        var videoList: MutableList<Video> = mutableListOf<Video>()
 )
