b1866a754c57e7068082f8e9de0a385050474b91
sea1hee <sea11hee@gmail.com>
2021-07-28 14:43:10 +0900

    [add] Repository에서 레트로핏 통신 관리하도록 클래스 추가 및 데이터 구조 변경, 코루틴 추가

---
+++ app/src/main/java/com/example/topgoal/db/Repository.kt
@@ -0,0 +1,71 @@
+package com.example.topgoal.db
+
+import android.util.Log
+import com.example.topgoal.model.Playlist
+import com.example.topgoal.model.Video
+import kotlinx.coroutines.*
+
+class Repository {
+
+    val TAG = "Repository"
+    private val API_KEY: String = "//"
+    private val ChannelId: String = "UCg09VbrEk1N0WrHflZi2rDA"
+
+    suspend fun getPlaylist(): List<Playlist> {
+        val RetCo = CoroutineScope(Dispatchers.IO).async {
+            val response: PlayList = PlayListService.client!!.getPlayList(
+                    API_KEY,
+                    "snippet",
+                    ChannelId
+            )
+            response
+        }
+        val response = RetCo.await()
+        val playlist = mutableListOf<Playlist>()
+        val ReCo2 = CoroutineScope(Dispatchers.IO).async {
+            for (i: Int in 0..response?.pageInfo!!.totalResults - 1) {
+                withContext(Dispatchers.Main) {
+                    playlist.add(
+                            Playlist(
+                                    response.items.get(i).id,
+                                    response.items.get(i).snippet.title
+                            )
+                    )
+                }
+                playlist[i].videoList.addAll(loadVideoList(response.items.get(i).id))
+            }
+            playlist
+        }
+        return ReCo2.await()
+    }
+
+
+    suspend fun loadVideoList(id: String): List<Video> {
+        val RetCo = CoroutineScope(Dispatchers.IO).async {
+            val response: PlayListItems = PlayListService.client!!.getPlayListItems(
+                    API_KEY,
+                    "snippet",
+                    id,
+                    50
+            )
+            response
+        }
+        val videoList = mutableListOf<Video>()
+        val response = RetCo.await()
+        val RetCo2 = CoroutineScope(Dispatchers.IO).async {
+            val indicator =
+                    if (response?.pageInfo!!.totalResults < 50) response?.pageInfo!!.totalResults - 1 else 49
+            for (i: Int in 0..indicator) {
+                videoList.add(
+                        Video(
+                                response.items[i].snippet.resourceId.videoId,
+                                response.items[i].snippet.title,
+                                response.items[i].snippet.thumbnails.default.url
+                        )
+                )
+            }
+            videoList
+        }
+        return RetCo2.await()
+    }
+}
--- app/src/main/java/com/example/topgoal/viewmodel/PlayListViewModel.kt
+++ app/src/main/java/com/example/topgoal/viewmodel/PlayListViewModel.kt
@@ -4,15 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.topgoal.db.PlayListService
-import com.example.topgoal.db.PlayList
-import com.example.topgoal.db.PlayListItems
+import com.example.topgoal.db.Repository
 import com.example.topgoal.model.Playlist
-import com.example.topgoal.model.Video
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class PlayListViewModel: ViewModel() {
 
@@ -21,59 +15,13 @@ class PlayListViewModel: ViewModel() {
     private val _playList = MutableLiveData<List<Playlist>>()
     var playList: LiveData<List<Playlist>> = _playList
 
-    private val API_KEY: String = "//"
-    private val ChannelId: String = "UCg09VbrEk1N0WrHflZi2rDA"
+    val repository = Repository()
 
     init {
         viewModelScope.launch {
-            val response: PlayList = PlayListService.client!!.getPlayList(
-                    API_KEY,
-                    "snippet",
-                    ChannelId
-            )
-            for (i: Int in 0..response?.pageInfo!!.totalResults - 1) {
-                withContext(Main) {
-                    addPlaylist(
-                            Playlist(
-                                    response.items.get(i).id,
-                                    response.items.get(i).snippet.title
-                            )
-                    )
-                }
-                loadVideoList(i)
-            }
+            PlayList.addAll(repository.getPlaylist().toMutableList())
+            _playList.value = PlayList
         }
-    }
-
-
-    fun loadVideoList(position: Int) {
-        PlayList[position].videoList = mutableListOf()
-        viewModelScope.launch {
-            val response: PlayListItems = PlayListService.client!!.getPlayListItems(
-                API_KEY,
-                "snippet",
-                PlayList[position].id,
-                50
-            )
-            val indicator =
-                if (response?.pageInfo!!.totalResults < 50) response?.pageInfo!!.totalResults - 1 else 49
-            for (i: Int in 0..indicator) {
-                    PlayList[position].videoList!!.add(
-                        Video(
-                            response.items[i].snippet.resourceId.videoId,
-                            response.items[i].snippet.title,
-                            response.items[i].snippet.thumbnails.default.url
-                        )
-                    )
-                    _playList.postValue(PlayList)
-            }
-        }
-    }
-
-
-    fun addPlaylist(newPlayList: Playlist){
-        PlayList.add(newPlayList)
-        _playList.value = PlayList
     }
 
     fun getPlaylist(position: Int): Playlist?{
