2c03aa2d2d2e0ea8f6d87bab6e36d9e16c085f3c
Lim <ljy7223@khu.ac.kr>
2021-08-23 14:58:59 +0900

    [add] Google 로그인 기능

--- app/src/main/java/com/example/topgoal/LoginActivity.kt
+++ app/src/main/java/com/example/topgoal/LoginActivity.kt
@@ -7,11 +7,20 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.topgoal.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -19,28 +28,70 @@ class LoginActivity : AppCompatActivity() {
     private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
             if (it.data?.getStringExtra("logout") != null) {
+                googleSignInClient.signOut()
+
                 Toast.makeText(this, R.string.toast_01, Toast.LENGTH_LONG).show()
             }
             else if (it.data?.getStringExtra("delete") != null) {
+                googleSignInClient.revokeAccess()
+
                 Toast.makeText(this, R.string.toast_02, Toast.LENGTH_LONG).show()
             }
         }
     }
 
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private val tag = "LoginActivity"
+    private val handleSignInResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            Log.d(tag, "firebaseAuthWithGoogle:" + account.id)
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException) { Log.w(tag, "GoogleSignIn:failure", e) }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("name", "사용자 이름")
-        intent.putExtra("email", "이메일")
-        intent.putExtra("photoUrl", "사진")
-        binding.button.setOnClickListener { startForResult.launch(intent) }
+        binding.button.setOnClickListener { handleSignInResult.launch(googleSignInClient.signInIntent) }
 
-        val customView = CustomView(this)
-        binding.constraintLayout.addView(customView)
+        binding.constraintLayout.addView(CustomView(this))
+
+        // Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // Firebase Auth
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateUI()
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(tag, "signInWithCredential:success")
+                        updateUI()
+                    }
+                    else Log.w(tag, "signInWithCredential:failure", task.exception)
+                }
+    }
+
+    private fun updateUI() {
+        if (Firebase.auth.currentUser != null) {
+            startForResult.launch(Intent(this, MainActivity::class.java))
+        }
     }
 }
 
